1556c3c9205a187792ed91b29e06327c9f208a08
Grigore Scutelnic <scutelnic.grigore10@gmail.com>
2023-10-13 17:53:20 +0300

    Crearea clasei Ceasca si lucrul cu ea

---
+++ src/Ceasca.java
@@ -0,0 +1,23 @@
+public class Ceasca {
+    private String forma;
+    private String culoare;
+    private String material;
+    private double volumL;
+    private boolean toarta;
+
+    Ceasca (String forma, String culoare, String material, double volumL, boolean toarta) {
+        this.forma = forma;
+        this.culoare = culoare;
+        this.material = material;
+        this.volumL = volumL;
+        this.toarta = toarta;
+    }
+
+    public void afis() {
+        System.out.println("Forma: " + forma);
+        System.out.println("Culoarea: " + culoare);
+        System.out.println("Meterialul: " + material);
+        System.out.println("Volumul: " + volumL);
+        System.out.println("Toarta: " + toarta);
+    }
+}
